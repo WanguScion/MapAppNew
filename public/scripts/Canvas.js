78f5259d16b7b372a1c400a2f697820df06ec5fb
WanguScion <scionwangu942@gmail.com>
2026-03-17 21:43:02 +0530

    Panning and Zooming intermediate

--- public/scripts/Canvas.js
+++ public/scripts/Canvas.js
@@ -8,8 +8,13 @@ var canvas = document.querySelector("canvas");
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
+var zoomIn = document.querySelector(".zoomIn");
+var zoomOut = document.querySelector(".zoomOut");
+
 let drawPosnX = 0;
 let drawPosnY = 0;
+let drawSize = 256;
+let drawScale = 1;
 
 const input = new InputHandler();
 var c = canvas.getContext("2d");
@@ -31,6 +36,18 @@ canvas.addEventListener("mousemove", (event) => {
   input.mouseX = event.clientX;
   input.mouseY = event.clientY;
 });
+canvas.addEventListener("wheel", (event) => {
+  event.preventDefault();
+  if (event.deltaY > 0) input.hasZoomedIn = true;
+  else if (event.deltaY < 0) input.hasZoomedOut = true;
+}, { passive: false });
+
+zoomIn.onclick = () => {
+  input.hasZoomedIn = true;
+};
+zoomOut.onclick = () => {
+  input.hasZoomedOut = true;
+};
 
 // Call images from tile server
 const img = new Image();
@@ -39,6 +56,7 @@ img.src = "https://tile.openstreetmap.org/0/0/0.png";
 // Draw On Canvas
 function updateCanvas() {
   c.clearRect(0, 0, innerWidth, innerHeight);
+
   // Controls Calc
   if (input.canPan) {
     drawPosnX += input.mouseX - input.mousePrevX;
@@ -46,7 +64,23 @@ function updateCanvas() {
   }
   input.mousePrevX = input.mouseX;
   input.mousePrevY = input.mouseY;
-  c.drawImage(img, 0, 0, 256, 256, drawPosnX, drawPosnY, 256, 256);
+
+  if (input.hasZoomedIn) {
+    drawPosnX = 2 * drawPosnX - (input.mouseX - (innerWidth-canvasWidth)/2);
+    drawPosnY = 2 * drawPosnY - (input.mouseY - (innerHeight-canvasHeight)/2);
+    drawScale++;
+    input.hasZoomedIn = false;
+  }
+  if (input.hasZoomedOut) {
+    if (drawScale > 1) {
+      drawPosnX = (drawPosnX + input.mouseX - (innerWidth-canvasWidth)/2) / 2;
+      drawPosnY = (drawPosnY + input.mouseY - (innerHeight-canvasHeight)/2) / 2;
+      drawScale--;
+    }
+    input.hasZoomedOut = false;
+  }
+
+  c.drawImage(img, 0, 0, 256, 256, drawPosnX, drawPosnY, drawSize*drawScale, drawSize*drawScale);
   requestAnimationFrame(updateCanvas);
 }
 requestAnimationFrame(updateCanvas);
--- public/scripts/Classes.js
+++ public/scripts/Classes.js
@@ -3,12 +3,21 @@ export class InputHandler {
   mousePrevX;
   mouseY;
   mousePrevY;
+
   canPan;
+
+  zoomIn;
+  zoomOut;
+
   constructor() {
     this.mouseX = 0;
     this.mousePrevX = 0;
     this.mouseY = 0;
     this.mousePrevY = 0;
+
     this.canPan = false;
+
+    this.hasZoomedIn = false;
+    this.hasZoomedOut = false;
   }
 }
